16d91723e707a83da13c5682ade59ceff957e684
Ricardo Antonio-arte <ricardoazm23@gmail.com>
2024-12-04 14:10:48 -0600

    Consegui hacer que la computadora devuelva Piedra Papel o Tijeras segun el numero generado

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+//FUNCION QUE CREA NUMEROS ENTRE 1 Y 3
 getComputerChoice = ( ) =>{ 
     return Math.floor( Math.random() *3 )+1;
-};
 
-console.log( getComputerChoice() );
+};
+//ESE NUMERO ALEATORIO LO ASIGNAMOS A UNA VARIABLE
+let randomnumber = getComputerChoice();
+
+
+//CON UN SWITCH "CONVERTIMOS" ESOS NUMEROS ALETORIOS EN CADENAS DE TEXTO
+switch( randomnumber ){ 
+    
+    case 1:{
+        console.log("Piedra");
+    }
+    break;
+
+    case 2:{
+        console.log("Papel");
+    }
+    break;
+
+    case 3: { 
+        console.log("Tijera");
+    }
+    break;
+}
+
+//console.log( randomnumber);
