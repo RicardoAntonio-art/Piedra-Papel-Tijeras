b6e0e1e887ba01635e1c4395716974af040608bd
Ricardo Antonio-arte <ricardoazm23@gmail.com>
2024-12-08 12:16:11 -0600

    Hacemos que la seleccion de la computadora se genere dentro de la funcion playround, lo cual hace que el programa genere el numero aleatorio cada que se da click en un boton

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 //FUNCION QUE CREA NUMEROS ENTRE 1 Y 3
-getComputerChoice = ( ) =>{ 
+function getComputerChoice ( ) { 
     return Math.floor( Math.random() *3 )+1;
 
 };
-//ESE NUMERO ALEATORIO LO ASIGNAMOS A UNA VARIABLE
-let randomnumber = getComputerChoice();
+//GUARDAMOS EL TEXTO EN UNA VARIABLE
 let computerChoice;
 
 
 //CON UN SWITCH "CONVERTIMOS" ESOS NUMEROS ALETORIOS EN CADENAS DE TEXTO
-switch( randomnumber ){ 
-    
-    case 1:
-        computerChoice = "piedra";    
-    break;
 
-    case 2:
-        computerChoice ="papel";    
-    break;
-
-    case 3: 
-        computerChoice = "tijera";    
-    break;
-}
+function getComputerChoiceName(computerChoice) {
+    switch (computerChoice) {
+      case 1:
+        return "piedra";
+      case 2:
+        return "papel";
+      case 3:
+        return "tijera";
+    }
+  }
 
 //BLOQUE QUE GUARDA LA ELECCION DEL JUGADOR
-
-let prueba = 12;
 
 const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
@@ -35,6 +29,9 @@ const cont = document.querySelector("#container");
 const result1 = document.createElement("p");
 const result2 = document.createElement("p");
 const winner = document.createElement("h1");
+const results = document.createElement("h1");
+const selections = document.createElement("h2");
+
 
 
     btn1.addEventListener("click",  () => {   
@@ -44,93 +41,89 @@ const winner = document.createElement("h1");
 
     btn2.addEventListener("click",  () => {   
         humanSelection = "papel";
-        //PlayRound(humanSelection, computerSelection);
+        PlayRound(humanSelection, computerSelection);
   });
 
   btn3.addEventListener("click",  () => {   
     humanSelection = "tijera";
-    //PlayRound(humanSelection, computerSelection);
+    PlayRound(humanSelection, computerSelection);
 });
-/*
-    btn2.addEventListener("click", () => {   
-        humanSelection = btn2.textContent; 
-        console.log(getHumanChoice);       
-    }); 
 
-    btn3.addEventListener("click", () => {   
-        humanSelection = btn3.textContent; 
-        console.log(getHumanChoice );      
-    }); 
-
-
-
-
-
-*/
 var humanSelection;
 const computerSelection = computerChoice;
 
 
-//ESTE ES EL INICIO DE UNA SOLA RONDA
-
 let ComputerScore = 0;
 let HumanScore = 0;
-PlayRound = (humanchoice, computerchoice) =>{ 
-    console.log( "El humano escogio " + humanchoice + " y la maquina " + computerchoice);
+PlayRound = (humanchoice, ) =>{ 
+    const computerChoiceNumber = getComputerChoice();
+    const computerChoice = getComputerChoiceName(computerChoiceNumber);
+    selections.textContent = "El humano escogio " + humanchoice + " y la maquina " + computerChoice;   
 
-    if( humanchoice === "piedra" && computerchoice === "papel"){         
+    if( humanchoice === "piedra" && computerChoice === "papel"){         
         ComputerScore++;
         winner.textContent = "La maquina gana!";
         result1.textContent = "Puntuacion de la Maquina: " + ComputerScore;
         result2.textContent = "Puntuacion del humano: "+ HumanScore;
-        cont.appendChild(winner);
-        cont.appendChild(result1);
+        cont.appendChild(selections);
         cont.appendChild(result2);
+        cont.appendChild(result1);
+        cont.appendChild(winner);
 
-    } else if ( humanchoice === "papel" && computerchoice === "tijera"){
+    } else if ( humanchoice === "papel" && computerChoice === "tijera"){
         ComputerScore++;
-        console.log("La maquina gana! su puntuacion es de: " + ComputerScore);
-        console.log("Puntuacion del humano: " + ComputerScore);
-    } else if ( humanchoice === "tijera" && computerchoice === "piedra"){ 
+        winner.textContent = "La maquina gana!";
+        result1.textContent = "Puntuacion de la Maquina: " + ComputerScore;
+        result2.textContent = "Puntuacion del humano: "+ HumanScore;
+        cont.appendChild(selections);
+        cont.appendChild(result2);
+        cont.appendChild(result1);
+        cont.appendChild(winner);
+
+    } else if ( humanchoice === "tijera" && computerChoice === "piedra"){ 
         ComputerScore++;
-        console.log("La maquina gana! su puntuacion es de: " + ComputerScore);
-        console.log("Puntuacion del humano: " + HumanScore);
-    }else if ( humanchoice === computerchoice){ 
-        console.log("Es un empate");
+        winner.textContent = "La maquina gana!";
+        result1.textContent = "Puntuacion de la Maquina: " + ComputerScore;
+        result2.textContent = "Puntuacion del humano: "+ HumanScore;
+        cont.appendChild(selections);
+        cont.appendChild(result2);
+        cont.appendChild(result1);
+        cont.appendChild(winner);
+
+    }else if ( humanchoice === computerChoice){ 
+        winner.textContent = "Es un empate";
+        result1.textContent = "Puntuacion de la Maquina: " + ComputerScore;
+        result2.textContent = "Puntuacion del humano: "+ HumanScore;
+        cont.appendChild(selections);
+        cont.appendChild(result2);
+        cont.appendChild(result1);
+        cont.appendChild(winner);
     }
      else {
         HumanScore++;
-        console.log("El humano gana! su puntuacion es de: " + HumanScore);
-        console.log("Puntuacion de la Maquina: " + ComputerScore);
+        winner.textContent = "El humano gana!"
+        result2.textContent = "Puntuacion del Humano: " + HumanScore;
+        result1.textContent = "Puntuacion de la Maquina: " + ComputerScore;
+        cont.appendChild(selections);
+        cont.appendChild(result2);
+        cont.appendChild(result1);
+        cont.appendChild(winner);
     }
 }
-
-
-/*
-PlayGame = ()=> {
-   
-    
-}
-
-PlayGame();
-PlayGame();
-PlayGame();
-PlayGame();
-PlayGame();
-
 //COMPARAMOS LAS PUNTUACIONES FINALES
 if( ComputerScore > HumanScore ){ 
-    console.log("La maquina gana la partida!");
+    results.textContent = "La maquina gana la pa"
 } else if ( HumanScore > ComputerScore){
-    console.log("El Humano Gano la partida");
+    results.textContent = "El humano gana la partida!";
+    cont.appendChild(results);
 }else{ 
-    console.log("FUE UN EMPATE");
+    cont.appendChild(results);
 }
 
 
 
 
 
-*/                                     
+                                   
 
 
